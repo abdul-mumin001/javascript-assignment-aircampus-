66ef675d6676a078040e789c6867ecc67ae3a1bc
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-19 01:41:03 +0530

    output3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <img src="output1.png" alt="output-1">
 <img src="output2.png" alt="output-2">
+<img src="output3.png" alt="output-3">
--- abdul.js
+++ abdul.js
@@ -2,7 +2,7 @@
 
 // let n=parseInt(process.argv[2]);
 
-// for(let i=1;i<n;i++){
+// for(let i=1;i<=n;i++){
 //     if(i%2!==0){
 //         console.log(i)
 //     }
@@ -10,13 +10,28 @@
 
 // 2Q. Print the sum of all even numbers in a range from a to b. (Including a,b)
 
-let a=parseInt(process.argv[2]);
-let b=parseInt(process.argv[3]);
-let sum=0;
-for(i=a;i<=b;i++){
-    if(i%2==0){
-        sum+=i;  
+// let a=parseInt(process.argv[2]);
+// let b=parseInt(process.argv[3]);
+// let sum=0;
+// for(i=a;i<=b;i++){
+//     if(i%2==0){
+//         sum+=i;  
        
-    }
+//     }
     
-}console.log(sum)
+// }console.log(sum)
+
+//3Q. Write a program to read the first and last digit of a number
+
+let num=parseInt(process.argv[2]);
+function firsDigit(num){
+    while(num>=10)
+    num=Math.floor(num/10);
+    return num;
+}
+function lastDigit(num){
+    num=Math.floor(num%10)
+    return num;
+}
+console.log(firsDigit(num)+" "+lastDigit(num));
+
