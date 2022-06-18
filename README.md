b93bc3892fa3a95486961fb17b5813cc224b67a1
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-19 00:15:18 +0530

    output2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <img src="output1.png" alt="output-1">
+<img src="output2.png" alt="output-2">
--- abdul.js
+++ abdul.js
@@ -1,11 +1,22 @@
 //1Q. Using a loop print all odd numbers  upto and including n.Don't include zero
 
-let n=parseInt(process.argv[2]);
+// let n=parseInt(process.argv[2]);
 
-for(let i=1;i<n;i++){
-    if(i%2!==0){
-        console.log(i)
-    }
-}
+// for(let i=1;i<n;i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+// }
 
 // 2Q. Print the sum of all even numbers in a range from a to b. (Including a,b)
+
+let a=parseInt(process.argv[2]);
+let b=parseInt(process.argv[3]);
+let sum=0;
+for(i=a;i<=b;i++){
+    if(i%2==0){
+        sum+=i;  
+       
+    }
+    
+}console.log(sum)
