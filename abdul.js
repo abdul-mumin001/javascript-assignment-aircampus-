175170a09ed15be024c6dc8f4cdc4b21490922fe
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-21 01:47:38 +0530

    5th

--- abdul.js
+++ abdul.js
@@ -38,17 +38,34 @@
 
 //4Q.Check whether the given number is armstrong or not
 
-const num=parseInt(process.argv[2]);
-const n=num.length;
-let sum=0;
-let x= num;
-while(x>0){
-    let rem=x%10;
-    sum+=rem**n;
-    x=parseInt(x/10)
-}
-if(sum==num){
-    console.log("is an armstrong")
-}else{
-    console.log("is not an armstrong")
-}
+// const num=parseInt(process.argv[2]);
+// const n=num.length;
+// let sum=0;
+// let x= num;
+// while(x>0){
+//     let rem=x%10;
+//     sum+=rem**n;
+//     x=parseInt(x/10)
+// }
+// if(sum==num){
+//     console.log("is an armstrong")
+// }else{
+//     console.log("is not an armstrong")
+// }
+
+//6Q. Take a number and return all even digit of numbers
+
+function even_digits(num) {
+    var x = 0;
+    while (num) {
+      x += num % 2 === 0;
+      num = Math.floor(num / 10);
+    }
+    return x;
+  }
+  
+  console.log(even_digits(7860));
+ 
+
+
+
